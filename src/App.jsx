b202a1e19e7dec572892bad152be6fe56923d0a6
Okolie Samuel <okechukwu.okolie.2023@gmail.com>
2026-02-18 15:52:43 +0100

    adding to the code base

--- src/App.jsx
+++ src/App.jsx
@@ -123,7 +123,7 @@ function App() {
             </div>
             <nav className="hidden md:flex gap-2">
               <button
-                className="px-3 py-2 rounded-lg bg-white/3 text-white dark:text:black"
+                className="px-3 py-2 rounded-lg bg-black text-white dark:text:slate-900"
                 onClick={() => {
                   setRoute("app");
                   setMobileMenuOpen(false);
